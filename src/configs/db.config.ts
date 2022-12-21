4bfc3c0c1c6cf1f9911dd1c356dc65417ecd6803
Quang Hiệp <quanghiep03198@gmail.com>
2022-12-21 19:42:48 +0700

    rebuild

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -2,7 +2,7 @@ import mongoose, { MongooseError } from "mongoose";
 import "dotenv/config";
 const connectMongoDB = async () => {
 	try {
-		const databaseUri = process.env.VERCEL_ENV == "development" ? process.env.LOCAL_DB_URI! : process.env.DB_URI!;
+		const databaseUri = process.env.NODE_ENV == "development" ? process.env.LOCAL_DB_URI! : process.env.DB_URI!;
 		mongoose.set("strictQuery", true);
 		await mongoose.connect(databaseUri);
 		console.log("Connected to database");
