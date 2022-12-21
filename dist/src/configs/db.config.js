8e5724bb66c191235201f09551e4d6fdd9556882
Quang Hiệp <quanghiep03198@gmail.com>
2022-12-21 22:05:10 +0700

    rebuild

--- dist/src/configs/db.config.js
+++ dist/src/configs/db.config.js
@@ -15,10 +15,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 require("dotenv/config");
 const connectMongoDB = () => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
     try {
-        // console.log(ip.);
-        const databaseUri = process.env.NODE_ENV == "production" ? process.env.DB_URI : process.env.LOCAL_DB_URI;
-        console.log(process.env.NODE_ENV);
+        const isProductionEnv = ((_a = process.env.NODE_ENV) === null || _a === void 0 ? void 0 : _a.indexOf("production")) != -1;
+        console.log("current node env:>>", isProductionEnv);
+        const databaseUri = isProductionEnv ? process.env.DB_URI : process.env.LOCAL_DB_URI;
+        console.log(databaseUri);
         mongoose_1.default.set("strictQuery", true);
         yield mongoose_1.default.connect(databaseUri);
         console.log("Connected to database");
