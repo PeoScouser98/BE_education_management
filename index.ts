f9d93989b0a3b56ed91a32a6cc0a9142c386b80d
Quang Hiệp <quanghiep03198@gmail.com>
2022-12-21 19:57:51 +0700

    rebuild

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
-	if (process.env.NODE_ENV === "DEVELOPMENT") {
+	if (process.env.NODE_ENV !== "production") {
 		console.log(`Server is listening on: http://localhost:${PORT}`);
 	}
 	connectSocketIO(server);
