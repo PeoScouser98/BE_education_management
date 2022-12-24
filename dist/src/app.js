61ec6f84026b8150853cc45894558d141deddee6
Quang Hiệp <quanghiep03198@gmail.com>
2022-12-24 19:17:10 +0700

    api document

--- dist/src/app.js
+++ dist/src/app.js
@@ -8,12 +8,16 @@ const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const student_route_1 = __importDefault(require("./routes/student.route"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const yamljs_1 = __importDefault(require("yamljs"));
 const app = (0, express_1.default)();
+const yamlFile = yamljs_1.default.load("./src/docs.yaml");
 // * Using middlewares
 app.use(express_1.default.json());
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
 app.use((0, morgan_1.default)("tiny"));
+app.use("/api/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(yamlFile));
 // * Using router
 app.use("/api", student_route_1.default);
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,20 @@ import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import studentRouter from "./routes/student.route";
+import swaggerUI from "swagger-ui-express";
+import yaml from "yamljs";
+import path from "path";
 
 const app = express();
+
+const yamlFile = yaml.load("./src/docs.yaml");
 
 // * Using middlewares
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("tiny"));
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(yamlFile));
 
 // * Using router
 app.use("/api", studentRouter);
