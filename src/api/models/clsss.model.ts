11f868105566524a0c77b4895a86abefc401626b
Quang Hiệp <quanghiep03198@gmail.com>
2022-12-27 20:12:57 +0700

    create class model

---
+++ src/api/models/clsss.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { Class } from "../interfaces/schemas.interface";
+
+const classSchema = new mongoose.Schema<Class>({
+	className: {
+		type: String,
+		require: true,
+		trim: true,
+		uppercase: true,
+	},
+	grade: {
+		type: String,
+		enum: ["1", "2", "3", "4", "5"],
+		require: true,
+	},
+	headTeacher: {
+		type: mongoose.Types.ObjectId,
+		ref: "Teachers",
+	},
+});
+
+export default mongoose.model("Classes", classSchema);
