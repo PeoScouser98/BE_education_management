4e433fb96c15b776ce32e3370d121793ec9736e5
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-08 04:26:34 +0700

    refactor headmaster service

--- src/api/services/headMaster.service.ts
+++ src/api/services/headMaster.service.ts
@@ -1,11 +1,11 @@
+import createHttpError from 'http-errors';
 import HeadmasterModel, { Headmaster } from '../models/headmaster.model';
 
 export const authenticateHeadmaster = async (payload: Pick<Headmaster, 'email' | 'password'>) => {
 	try {
 		const headmasterInfo = await HeadmasterModel.findOne({ email: payload.email }).exec();
-		if (!headmasterInfo) throw new Error("Account doesn't exist!");
-		if (!headmasterInfo.authenticate(payload.password)) throw new Error('Incorrect password!');
-		console.log(headmasterInfo);
+		if (!headmasterInfo) throw createHttpError.NotFound("Account doesn't exist!");
+		if (!headmasterInfo.authenticate(payload.password)) throw createHttpError.BadRequest('Incorrect password!');
 		return headmasterInfo;
 	} catch (error) {
 		throw error as Error;
