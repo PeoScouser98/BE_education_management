51b916efda2531f8d7f870beb38da2f869ca7cd4
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 00:27:09 +0700

    update class model

--- src/api/models/class.model.ts
+++ src/api/models/class.model.ts
@@ -1,40 +1,39 @@
-import mongoose, { ObjectId } from "mongoose";
-import mongooseAutoPopulate from "mongoose-autopopulate";
+import mongoose, { ObjectId } from 'mongoose';
+import mongooseAutoPopulate from 'mongoose-autopopulate';
 
 export interface Class extends Document {
-    _id: ObjectId;
-    grade: ObjectId;
-    className: string;
-    headTeacher: ObjectId;
-    students: Array<ObjectId>;
+	_id: ObjectId;
+	grade: ObjectId;
+	className: string;
+	headTeacher: ObjectId;
+	students: Array<ObjectId>;
 }
 
 const ClassSchema = new mongoose.Schema<Class>({
-    className: {
-        type: String,
-        require: true,
-        trim: true,
-        uppercase: true,
-    },
-    grade: {
-        type: Number,
-        enum: [1, 2, 3, 4, 5],
-        require: true,
-    },
-    headTeacher: {
-        type: mongoose.Types.ObjectId,
-        ref: "Teachers",
-        autopopulate: { select: "_id fullName phone email" },
-    },
-    students: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: "Students",
-            autopopulate: { select: "studentName" },
-        },
-    ],
+	className: {
+		type: String,
+		require: true,
+		trim: true,
+		uppercase: true,
+	},
+	grade: {
+		type: Number,
+		enum: [1, 2, 3, 4, 5],
+		require: true,
+	},
+	headTeacher: {
+		type: mongoose.Types.ObjectId,
+		ref: 'Teachers',
+		autopopulate: { select: '_id fullName phone email' },
+	},
 });
 
-const ClassModel = mongoose.model<Class>("Classes", ClassSchema);
+ClassSchema.virtual('students', {
+	localField: '_id',
+	foreignField: 'class',
+	ref: 'students',
+});
+
+const ClassModel = mongoose.model<Class>('Classes', ClassSchema);
 
 export default ClassModel;
