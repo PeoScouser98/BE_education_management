8979d5089c26e4f530d08c9e40e74d131216d420
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 03:00:33 +0700

    user router

---
+++ src/api/routes/user.route.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { checkAuthenticated, checkIsHeadmaster } from '../middlewares/authGuard.middleware';
+import * as HeadmasterControler from '../controllers/user.controller';
+const router = express.Router();
+
+router.post(
+	'/create-teacher-account',
+	checkAuthenticated,
+	checkIsHeadmaster,
+	HeadmasterControler.createTeacherAccount
+);
+router.post('/create-parents-account');
+export default router;
