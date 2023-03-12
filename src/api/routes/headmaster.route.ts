50dd6b2036690fdff742b87943bfe468f71861ce
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 03:11:56 +0700

    delete

--- src/api/routes/headmaster.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { checkIsHeadmaster } from '../middlewares/checkAuth.middleware';
-import * as HeadmasterControler from '../controllers/headmaster.controller';
-const router = express.Router();
-
-router.post('/auth/create-teacher-account', checkIsHeadmaster, HeadmasterControler.createTeacherAccount);
-router.post('/auth/signin-as-headmaster', HeadmasterControler.signinAsHeadmaster);
-export default router;
