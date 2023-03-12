a8518da754888ed6f498531c7ae1a2ba22161d8b
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 03:00:13 +0700

    update subject controller

--- src/api/controllers/subject.controller.ts
+++ src/api/controllers/subject.controller.ts
@@ -1,32 +1,29 @@
-import { Request, Response } from "express";
-import createHttpError from "http-errors";
-import * as SubjectServices from "../services/subject.service";
-import { HttpException } from "../../interfaces/error.interface";
+import { Request, Response } from 'express';
+import createHttpError, { HttpError } from 'http-errors';
+import * as SubjectServices from '../services/subject.service';
 
 export const list = async (req: Request, res: Response) => {
-    try {
-        const subjects = await SubjectServices.getAllSubjects();
-        if (!subjects) throw createHttpError.NotFound("Cannot get subjects!");
-        return res.status(200).json(subjects);
-    } catch (error) {
-        const { message, status } = error as unknown as HttpException;
-        return res.json({
-            status,
-            message,
-        });
-    }
+	try {
+		const subjects = await SubjectServices.getAllSubjects();
+		if (!subjects) throw createHttpError.NotFound('Cannot get subjects!');
+		return res.status(200).json(subjects);
+	} catch (error) {
+		return res.status((error as HttpError).status || 500).json({
+			message: (error as HttpError | Error).message,
+			statusCode: (error as HttpError).status || 500,
+		});
+	}
 };
 
 export const create = async (req: Request, res: Response) => {
-    try {
-        const newSubject = await SubjectServices.createNewSubject(req.body);
-        if (!newSubject) throw createHttpError.BadRequest("Cannot create new subject!");
-        return res.status(201).json(newSubject);
-    } catch (error) {
-        const { message, status } = error as unknown as HttpException;
-        return res.json({
-            status,
-            message,
-        });
-    }
+	try {
+		const newSubject = await SubjectServices.createNewSubject(req.body);
+		if (!newSubject) throw createHttpError.BadRequest('Cannot create new subject!');
+		return res.status(201).json(newSubject);
+	} catch (error) {
+		return res.status((error as HttpError).status || 500).json({
+			message: (error as HttpError | Error).message,
+			statusCode: (error as HttpError).status || 500,
+		});
+	}
 };
