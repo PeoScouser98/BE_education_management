48e2fdf6b0f63a536cc455bf19777f205aca5a58
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 00:26:38 +0700

    delete files

--- src/interfaces/schema.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-	email: string;
-	fullName: string;
-	password: string;
-	photoUrl: string;
-	phone: string;
-	authenticate: (password: string) => boolean;
-	encryptPassword: (password: string) => string;
-}
