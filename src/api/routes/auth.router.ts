bc7faf5f0d82e7a826b16f35349fe352d4a1e53d
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 03:01:13 +0700

    update auth router

--- src/api/routes/auth.router.ts
+++ src/api/routes/auth.router.ts
@@ -18,7 +18,7 @@ router.get('/auth/signin/success', AuthController.signinWithGoogle);
 
 router.post(
 	'/auth/signin',
-	passport.authenticate('local', { failureRedirect: `${process.env.CLIENT_URL}/signin` }),
+	// passport.authenticate('local', { failureRedirect: `${process.env.CLIENT_URL}/signin` }),
 	AuthController.signinWithPhoneNumber
 );
 router.get('/auth/signout', AuthController.signout);
