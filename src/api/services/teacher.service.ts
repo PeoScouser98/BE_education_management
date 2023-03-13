dc0b1eb44116a81b305bd6fb28e4ad2232a5806d
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 20:34:01 +0700

    teacher service

--- src/api/services/teacher.service.ts
+++
@@ -1,9 +0,0 @@
-import TeacherModel, { Teacher } from '../models/teacher.model';
-
-export const createNewTeacher = async (payload: Omit<Teacher, '_id' | 'employmentStatus'>) => {
-	try {
-		return await new TeacherModel(payload).save();
-	} catch (error) {
-		throw error;
-	}
-};
