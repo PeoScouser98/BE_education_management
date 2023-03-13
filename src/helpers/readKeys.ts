cb704a93ca557a1dd4d49b6d6bcf14a6143adb34
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-13 23:05:33 +0700

    rebuild

--- src/helpers/readKeys.ts
+++ src/helpers/readKeys.ts
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import path from 'path';
 
-export const publicKey = fs.readFileSync(path.resolve('public.pem'));
-export const privateKey = fs.readFileSync(path.resolve('private.pem'));
+export const publicKey = fs.readFileSync('public.pem');
+export const privateKey = fs.readFileSync('private.pem');
