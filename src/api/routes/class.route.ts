1de962bfb8a5083f004af7a9c2a52e75be94f44b
unknown <duclnph24870@fpt.edu.vn>
2023-03-16 04:28:18 +0700

    fix: add middleware router get class

--- src/api/routes/class.route.ts
+++ src/api/routes/class.route.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/classes', checkAuthenticated, checkIsHeadmaster, classController.createClass);
 router.put('/classes/:_id', checkAuthenticated, checkIsHeadmaster, classController.updateClass);
 router.delete('/classes/:_id', checkAuthenticated, checkIsHeadmaster, classController.removeClass);
-router.get('/classes', classController.getClasses);
-router.get('/class/:_id', classController.getClassOne);
+router.get('/classes', checkAuthenticated, classController.getClasses);
+router.get('/class/:_id', checkAuthenticated, classController.getClassOne);
 
 export default router;
