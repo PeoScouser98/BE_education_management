c516b0560a27cd980b540e8b2dee455b1113e740
unknown <duclnph24870@fpt.edu.vn>
2023-03-16 04:26:02 +0700

    feat: complete features get classes and get one class api

--- src/api/controllers/class.controller.ts
+++ src/api/controllers/class.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import * as ClassService from '../services/class.service';
 import createHttpError, { HttpError, isHttpError } from 'http-errors';
-import { Class } from '../models/class.model';
+import ClassModel, { Class } from '../models/class.model';
+import mongoose from 'mongoose';
 
 // [POST] /api/classes (create classes)
 export const createClass = async (req: Request, res: Response) => {
@@ -92,8 +93,69 @@ export const removeClass = async (req: Request, res: Response) => {
 				statusCode: error.status,
 			});
 		} else {
-			console.log(error);
+			return res.status(500).json(error);
+		}
+	}
+};
 
+// [GET] /api/classes?limit=10&page=1&sortProperties=className&sort=desc
+export const getClasses = async (req: Request, res: Response) => {
+	try {
+		const { limit = 10, page } = req.query;
+		const sortProperties: any = req.query.sortProperties || 'grade';
+		const sort: any = req.query.sort || 'asc';
+		if (!page) {
+			throw createHttpError.BadRequest('Missing parameter');
+		}
+
+		if (!['className', 'grade', 'createdAt', 'updatedAt'].includes(sortProperties)) {
+			throw createHttpError.BadGateway(
+				"sortProperties can only belong to ['className', 'grade','createdAt','updatedAt']"
+			);
+		}
+
+		let start = Number(page) === 1 ? 0 : Number(limit) * Number(page) - Number(limit);
+
+		const result = await ClassModel.find({})
+			.sort({ [sortProperties]: sort })
+			.skip(start)
+			.limit(Number(limit));
+
+		return res.status(200).json({
+			classes: result,
+			page: Number(page),
+			sort: [sortProperties, sort],
+		});
+	} catch (error) {
+		if (isHttpError(error)) {
+			return res.status(error.statusCode).json({
+				message: error.message,
+				statusCode: error.status,
+			});
+		} else {
+			return res.status(500).json(error);
+		}
+	}
+};
+
+// [GET] /api/class/:_id
+export const getClassOne = async (req: Request, res: Response) => {
+	try {
+		const id = req.params._id;
+		if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+			throw createHttpError.BadRequest('Missing parameter');
+		}
+
+		const classResult = await ClassModel.findOne({ _id: id });
+
+		return res.status(200).json(classResult);
+	} catch (error) {
+		if (isHttpError(error)) {
+			return res.status(error.statusCode).json({
+				message: error.message,
+				statusCode: error.status,
+			});
+		} else {
 			return res.status(500).json(error);
 		}
 	}
--- src/api/routes/class.route.ts
+++ src/api/routes/class.route.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/classes', checkAuthenticated, checkIsHeadmaster, classController.createClass);
 router.put('/classes/:_id', checkAuthenticated, checkIsHeadmaster, classController.updateClass);
 router.delete('/classes/:_id', checkAuthenticated, checkIsHeadmaster, classController.removeClass);
+router.get('/classes', classController.getClasses);
+router.get('/class/:_id', classController.getClassOne);
 
 export default router;
