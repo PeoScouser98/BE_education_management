328b3f6338861188f4fe9888345505261de73396
Maladroy <hoangchilam@protonmail.com>
2023-03-18 03:03:09 +0700

    #6 - create permission model

--- src/api/models/permission.model.ts
+++ src/api/models/permission.model.ts
@@ -1,1 +1,31 @@
-import mongoose from 'mongoose';
+import mongooseAutoPopulate from "mongoose-autopopulate";
+import mongoose, { ObjectId } from 'mongoose';
+
+export interface Permission extends Document {
+    _id: ObjectId;
+    role: string;
+    type: string;
+}
+
+const PermissionSchema = new mongoose.Schema<Permission>({
+    _id: {
+        type: mongoose.Types.ObjectId,
+        default: new mongoose.Types.ObjectId(),
+    },
+    role: {
+        type: String,
+        require: true,
+        //extra requirements if needed
+    },
+    type: {
+        type: String,
+        require: true,
+        //extra requirements if needed
+    },
+})
+
+PermissionSchema.plugin(mongooseAutoPopulate);
+
+const PermissionModel = mongoose.model<Permission>('Permissions', PermissionSchema);
+
+export default PermissionModel;
