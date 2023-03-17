b5d77f5ae5984084b5e1a800151e925ac9c26306
unknown <duclnph24870@fpt.edu.vn>
2023-03-17 19:41:48 +0700

    create features_students

--- src/api/controllers/student.controller.ts
+++ src/api/controllers/student.controller.ts
@@ -1,12 +1,15 @@
-import { Request, Response } from "express";
-import createHttpError, { HttpError } from "http-errors";
-import * as StudentServices from "../services/student.service";
+import { Request, Response } from 'express';
+import createHttpError, { HttpError } from 'http-errors';
+import * as StudentServices from '../services/student.service';
 
+// creat fetures_students
 export const list = async (req: Request, res: Response) => {
 	try {
-		const students = await StudentServices.getStudentsByClass(req.params.classId).catch((err) => {
-			throw createHttpError.NotFound("Cannot find any student!");
-		});
+		const students = await StudentServices.getStudentsByClass(req.params.classId).catch(
+			(err) => {
+				throw createHttpError.NotFound('Cannot find any student!');
+			}
+		);
 		return res.status(200).json(students);
 	} catch (error) {
 		return res.status((error as HttpError).status || 500).json({
@@ -19,7 +22,7 @@ export const list = async (req: Request, res: Response) => {
 export const read = async (req: Request, res: Response) => {
 	try {
 		return await StudentServices.getStudent(req.params.id).catch((err) => {
-			throw createHttpError.NotFound("Cannot find student!");
+			throw createHttpError.NotFound('Cannot find student!');
 		});
 	} catch (error) {
 		return res.status((error as HttpError).status || 500).json({
@@ -32,7 +35,7 @@ export const read = async (req: Request, res: Response) => {
 export const create = async (req: Request, res: Response) => {
 	try {
 		const newStudent = await StudentServices.createStudent(req.body);
-		console.log("new student:>>", newStudent);
+		console.log('new student:>>', newStudent);
 		return res.status(201).json(newStudent);
 	} catch (error) {
 		return res.status((error as HttpError).status || 500).json({
@@ -44,7 +47,7 @@ export const create = async (req: Request, res: Response) => {
 
 export const update = async (req: Request, res: Response) => {
 	try {
-		if (!req.body) throw createHttpError.BadRequest("Provide student data to update");
+		if (!req.body) throw createHttpError.BadRequest('Provide student data to update');
 		const updatedStudent = await StudentServices.updateStudent(req.body);
 		return res.status(201).json(updatedStudent);
 	} catch (error) {
