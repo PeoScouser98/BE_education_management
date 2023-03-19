cb18f184c7eb66d9fe8508278551eebdde93f3df
Maladroy <hoangchilam@protonmail.com>
2023-03-20 00:28:34 +0700

    #6 - fix: permission APIs missing user authentication

--- src/api/routes/permission.route.ts
+++ src/api/routes/permission.route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import * as permissionController from '../controllers/permission.controllers';
-import { checkIsHeadmaster } from '../middlewares/authGuard.middleware';
+import { checkIsHeadmaster, checkAuthenticated } from '../middlewares/authGuard.middleware';
 const router = express.Router();
 
-router.get('/permission/', permissionController.list);
-router.get('/permission/:id', permissionController.read);
-router.post('/permission/', checkIsHeadmaster, permissionController.create);
-router.put('/permission/:id', checkIsHeadmaster, permissionController.update);
-router.delete('/permission/:id', checkIsHeadmaster, permissionController.remove);
+router.get('/permission/', checkAuthenticated, permissionController.list);
+router.get('/permission/:id', checkAuthenticated, permissionController.read);
+router.post('/permission/', checkAuthenticated, checkIsHeadmaster, permissionController.create);
+router.put('/permission/:id', checkAuthenticated, checkIsHeadmaster, permissionController.update);
+router.delete('/permission/:id', checkAuthenticated, checkIsHeadmaster, permissionController.remove);
 
 export default router;
