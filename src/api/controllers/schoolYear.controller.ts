29538b57006d7a9f702a58d30a479341e594266f
unknown <duclnph24870@fpt.edu.vn>
2023-03-30 00:43:19 +0700

    #32 - @duclnph24870 feat: create api schoolyear and add description by schema

--- src/api/controllers/schoolYear.controller.ts
+++ src/api/controllers/schoolYear.controller.ts
@@ -1,61 +1,36 @@
 import { Request, Response } from 'express';
-import HttpError from 'http-errors';
-import * as schoolYearServices from '../services/schoolYear.service';
+import createHttpError, { HttpError } from 'http-errors';
+import { MongooseError } from 'mongoose';
+import * as SchoolYearServices from '../services/schoolYear.service';
 
-export const list = async (req: Request, res: Response) => {
+// [GET] /api/schoolYears?limit=10&page=1
+export const schoolYearList = async (req: Request, res: Response) => {
 	try {
-		const limit = req.query.limit ? req.query.limit : 10;
-		const currPage = req.query.page ? req.query.page : 1;
-		const data = await schoolYearServices.getAll(limit as number, currPage as number);
-		if (!data.schoolYears) throw new HttpError.NotFound('Cannot find any school year!');
-		return res.status(200).json({
-			schoolYears: data.schoolYears,
-			pages: data.pages,
+		const limit: number = req.query.limit ? Number(req.query.limit) : 10;
+		const page: number = req.query.page ? Number(req.query.page) : 1;
+		const result = await SchoolYearServices.getAllSchoolYear(limit, page);
+
+		return res.status(200).json(result);
+	} catch (error) {
+		return res.status((error as HttpError).statusCode || 500).json({
+			message: (error as HttpError | MongooseError).message,
+			statusCode: (error as HttpError).status || 500,
+			error: (error as any).error,
 		});
-	} catch (error) {
-		return res.status(500).json(error);
 	}
 };
 
-export const create = async (req: Request, res: Response) => {
+// [POST] /api/schoolYear
+export const createSchoolYear = async (req: Request, res: Response) => {
 	try {
-		const newSchoolYear = await schoolYearServices.create(req.body);
-		console.log('New school year', newSchoolYear);
-		return res.status(200).json(newSchoolYear);
-	} catch (error) {
-		return res.status(500).json(error);
-	}
-};
+		const result = await SchoolYearServices.createSchoolYear();
 
-export const update = async (req: Request, res: Response) => {
-	try {
-		const schoolYearId = req.params.schoolYearId;
-		if (schoolYearId) {
-			const newSchoolYear = await schoolYearServices.update(req.body, schoolYearId);
-			console.log('New school year', newSchoolYear);
-			return res.status(200).json({
-				message: 'Update school year successfully!',
-			});
-		} else {
-			throw HttpError.NotFound('Page not found!');
-		}
+		return res.status(201).json(result);
 	} catch (error) {
-		return res.status(500).json(error);
-	}
-};
-
-export const remove = async (req: Request, res: Response) => {
-	try {
-		const schoolYearId = req.params.schoolYearId;
-		if (schoolYearId) {
-			await schoolYearServices.remove(schoolYearId);
-			return res.status(200).json({
-				message: 'Delete school year successfully!',
-			});
-		} else {
-			throw HttpError.NotFound('Page not found!');
-		}
-	} catch (error) {
-		return res.status(500).json(error);
+		return res.status((error as HttpError).statusCode || 500).json({
+			message: (error as HttpError | MongooseError).message,
+			statusCode: (error as HttpError).status || 500,
+			error: (error as any).error,
+		});
 	}
 };
