0dfd06aade85ce25064bfbb0766c07a90eb0469a
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-15 15:51:20 +0700

    Update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	mode: 'production',
@@ -17,8 +18,17 @@ module.exports = {
 		rules: [
 			{
 				test: /\.ts$/,
-				use: 'swc-loader',
 				exclude: /node_modules/,
+				use: {
+					loader: 'swc-loader',
+					options: {
+						jsc: {
+							parser: {
+								syntax: 'typescript',
+							},
+						},
+					},
+				},
 			},
 		],
 	},
@@ -36,32 +46,5 @@ module.exports = {
 		],
 	},
 
-	externals: {
-		bcrypt: 'commonjs bcrypt',
-		compression: 'commonjs compression',
-		cors: 'commonjs cors',
-		dotenv: 'commonjs dotenv',
-		express: 'commonjs express',
-		googleapis: 'commonjs googleapis',
-		redis: 'commonjs redis',
-		joi: 'commonjs joi',
-		'express-session': 'commonjs express-session',
-		passport: 'commonjs passport',
-		'http-errors': 'commonjs http-errors',
-		jsonwebtoken: 'commonjs jsonwebtoken',
-		mongoose: 'commonjs mongoose',
-		'mongoose-autopopulate': 'commonjs mongoose-autopopulate',
-		'mongoose-delete': 'commonjs mongoose-delete',
-		'passport-google-oauth2': 'commonjs passport-google-oauth2',
-		'passport-local': 'commonjs passport-local',
-		swagger: 'commonjs swagger',
-		'swagger-jsdoc': 'commonjs swagger-jsdoc',
-		'swagger-ui-express': 'commonjs swagger-ui-express',
-		yaml: 'yaml',
-		morgan: 'commonjs morgan',
-		multer: 'commonjs multer',
-		helmet: 'commonjs helmet',
-		nock: 'commonjs2 nock',
-		'mock-aws-s3': 'commonjs2 mock-aws-s3',
-	},
+	externals: [nodeExternals()],
 };
