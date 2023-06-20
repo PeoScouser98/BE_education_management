2c4d419be99690e5468a54b109666c1105923af4
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-20 21:44:19 +0700

    Update school year request queries

--- src/api/controllers/schoolYear.controller.ts
+++ src/api/controllers/schoolYear.controller.ts
@@ -5,23 +5,20 @@ import useCatchAsync from '../../helpers/useCatchAsync';
 
 // [GET] /api/schoolYears?limit=10&page=1
 export const schoolYearList = useCatchAsync(async (req: Request, res: Response) => {
-	const limit: number = req.query.limit ? Number(req.query.limit) : 10;
-	const page: number = req.query.page ? Number(req.query.page) : 1;
+	const limit: number = req.query._limit ? Number(req.query._limit) : 10;
+	const page: number = req.query._page ? Number(req.query._page) : 1;
 	const result = await SchoolYearServices.getAllSchoolYear(limit, page);
-
 	return res.status(HttpStatusCode.OK).json(result);
 });
 
 // [GET] /api/schoolYears/current
 export const getCurrentYear = useCatchAsync(async (req: Request, res: Response) => {
 	const result = await SchoolYearServices.selectSchoolYearCurr();
-
 	return res.status(200).json(result);
 });
 
 // [POST] /api/schoolYear
 export const createSchoolYear = useCatchAsync(async (req: Request, res: Response) => {
 	const result = await SchoolYearServices.createSchoolYear();
-
 	return res.status(HttpStatusCode.CREATED).json(result);
 });
