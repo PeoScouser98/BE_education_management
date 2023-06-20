0afa66107f36fed7a76cb1defa1adfe93aec4e60
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-21 03:15:57 +0700

    (#90) Update student's transcript response data

--- src/api/routes/subjectTrancription.route.ts
+++ src/api/routes/subjectTrancription.route.ts
@@ -1,12 +1,27 @@
 import express from 'express';
 import * as TrancriptionController from '../controllers/subjectTrancription.controllers';
+import { checkAuthenticated, checkIsTeacher } from '../middlewares/authGuard.middleware';
 
 const router = express.Router();
 
-router.post('/transcripts/:classId/:subjectId', TrancriptionController.scoreTableInputs);
-router.post('/transcripts/:studentId/:subjectId', TrancriptionController.scoreTableInputOne);
-router.get('/transcripts/class/:classId/:subjectId', TrancriptionController.getTranscriptByClass);
-router.get('/transcripts/student/:id', TrancriptionController.getTranscriptByStudent);
-router.get('/transcript/subjectAll/:classId', TrancriptionController.selectTranscriptAllSubjectByClass);
+router.post(
+	'/transcripts/:classId/:subjectId',
+	checkAuthenticated,
+	checkIsTeacher,
+	TrancriptionController.scoreTableInputs
+);
+router.post(
+	'/transcripts/:studentId/:subjectId',
+	checkAuthenticated,
+	checkIsTeacher,
+	TrancriptionController.scoreTableInputOne
+);
+// router.get('/transcripts/class/:classId/:subjectId', TrancriptionController.getTranscriptByClass);
+router.get('/transcripts/student/:id', checkAuthenticated, TrancriptionController.getTranscriptByStudent);
+router.get(
+	'/transcript/subjectAll/:classId',
+	checkAuthenticated,
+	TrancriptionController.selectTranscriptAllSubjectByClass
+);
 
 export default router;
