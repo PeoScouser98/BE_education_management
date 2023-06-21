7722baf78416acf99c3773087c0b9d3a22e78131
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-21 23:24:03 +0700

    (#92) Feature: update user info

--- src/api/routes/user.route.ts
+++ src/api/routes/user.route.ts
@@ -9,24 +9,28 @@ router.post(
 	checkIsHeadmaster,
 	UserController.createTeacherAccount
 );
-
 router.post(
 	'/users/create-parents-account', //updated route name
 	checkAuthenticated,
 	checkIsTeacher,
 	UserController.createParentsAccount
 );
-
 router.patch(
 	'/users/teachers/:userId/deactivate', // updated route name
 	checkAuthenticated,
 	checkIsHeadmaster,
 	UserController.deactivateTeacherAccount
 );
-
 router.patch('/update-user', checkAuthenticated, UserController.updateUserInfo); // Users update themselve information
+router.patch(
+	'/users/:teacherId/update-teacher',
+	checkAuthenticated,
+	checkIsHeadmaster,
+	UserController.updateTeacherInfo
+);
+router.patch('/users/parents/:parentsId', checkAuthenticated, checkIsTeacher, UserController.updateParentsInfo);
 router.get('/users/teachers', checkAuthenticated, UserController.getTeachersByStatus);
-router.get('/users/:id', checkAuthenticated, checkIsHeadmaster, UserController.getUserDetails);
+router.get('/users/:userId', checkAuthenticated, UserController.getUserDetails);
 router.get('/users/parents/:classId', checkAuthenticated, UserController.getParentsUserByClass);
 router.post('/users/search-parents', checkAuthenticated, checkIsTeacher, UserController.searchParentsUsers);
 // router.patch(
@@ -35,6 +39,5 @@ router.post('/users/search-parents', checkAuthenticated, checkIsTeacher, UserCon
 // 	checkIsHeadmaster,
 // 	UserController.deactivateParentAccount
 // );
-// router.patch('/user/parents/:id', checkAuthenticated, checkIsTeacher, UserController.updateParentsInfo) // not yet
 
 export default router;
