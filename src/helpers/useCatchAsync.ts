5aaf920e65fd578a87cc6ae5fc2ceb71edc24ef2
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-29 23:11:15 +0700

    Update useCatchAsync helper

--- src/helpers/useCatchAsync.ts
+++ src/helpers/useCatchAsync.ts
@@ -1,11 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../types/httpException.type';
 
-type TAsyncFn = (
-	req: Request,
-	res: Response,
-	next?: NextFunction
-) => Promise<Response<any, Record<string, any>>> | Promise<any>;
+type TAsyncFn = (req: Request, res: Response, next?: NextFunction) => Promise<any>;
 
 const useCatchAsync = (fn: TAsyncFn) => (req: Request, res: Response) =>
 	Promise.resolve(fn(req, res)).catch((error) => {
