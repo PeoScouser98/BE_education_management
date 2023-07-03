253dd3a4e672e1a57761efc068b86879a5a9b2b3
quanghiep03198 <quanghiep03198@gmail.com>
2023-07-03 19:09:09 +0700

    (#153) Update create student remark by class

--- src/api/services/studentRemark.service.ts
+++ src/api/services/studentRemark.service.ts
@@ -1,28 +1,36 @@
 import createHttpError from 'http-errors';
 import { ObjectId } from 'mongoose';
 import { IStudentRemark } from '../../types/student.type';
-import StudentModel from '../models/student.model';
+import ClassModel from '../models/class.model';
 import StudentRemarkModel from '../models/studentRemark.model';
 import { validateNewStudentRemark } from '../validations/studentRemark.validation';
 import { getCurrentSchoolYear } from './schoolYear.service';
 
-export const createStudentRemark = async (data: Partial<IStudentRemark>, headTeacherId: string | ObjectId) => {
+export const createStudentRemarkEntireClass = async (
+	data: Array<Omit<IStudentRemark, '_id'>>,
+	headTeacherId: string | ObjectId,
+	classId: string
+) => {
 	const currentSchoolYear = await getCurrentSchoolYear();
-	const existedRemark = await StudentRemarkModel.findOne({ student: data.student, schoolYear: currentSchoolYear._id });
-	if (existedRemark) throw createHttpError.Conflict('Remark for this student in current school year already existed!');
-	const student = await StudentModel.findOne({ _id: data.student }).populate({
-		path: 'class',
-		select: '_id className headTeacher',
-		match: { headTeacher: headTeacherId }
-	});
-	if (!student?.class) throw createHttpError.BadRequest('Only head teacher can remark conduct for this student !');
-	const payload = {
-		...data,
-		schoolYear: currentSchoolYear._id?.toString(),
-		remarkedBy: headTeacherId
-	} as Omit<IStudentRemark, '_id'>;
+	const currentClass = await ClassModel.findOne({ _id: classId, headTeacher: headTeacherId });
+	if (!currentClass) throw createHttpError.Forbidden('Only head teacher can remark conduct for this student !');
+	const payload = data.map(
+		(item) =>
+			({
+				...item,
+				schoolYear: currentSchoolYear._id?.toString(),
+				remarkedBy: headTeacherId
+			} as Omit<IStudentRemark, '_id'>)
+	);
 	const { error, value } = validateNewStudentRemark(payload);
 	if (error) throw createHttpError.BadRequest(error.message);
-	const newStudentConduct = await new StudentRemarkModel(value).save();
-	return newStudentConduct;
+	const bulkWriteOptions = value.map((v: IStudentRemark) => ({
+		updateOne: {
+			filter: { student: v.student, schoolYear: currentSchoolYear },
+			update: v,
+			upsert: true
+		}
+	}));
+	await StudentRemarkModel.bulkWrite(bulkWriteOptions);
+	return { message: `Insert students's remark for this class successfully !` };
 };
