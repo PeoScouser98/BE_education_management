c1c85f72827f2e0fede29d24285f77050e74ef8e
quanghiep03198 <quanghiep03198@gmail.com>
2023-07-21 01:34:37 +0700

    Fix ts type error

--- src/api/services/attendance.service.ts
+++ src/api/services/attendance.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import createHttpError from 'http-errors'
 import moment from 'moment'
 import mongoose, { isValidObjectId } from 'mongoose'
@@ -7,6 +8,7 @@ import StudentModel from '../models/student.model'
 import { validateAttedancePayload } from '../validations/attendance.validation'
 import { AttendanceSessionEnum } from './../../types/attendance.type'
 import ClassModel from '../models/class.model'
+import { IStudent } from '../../types/student.type'
 
 export const saveAttendanceByClass = async (payload: Array<Omit<IAttendance, '_id'>>, session: string) => {
 	const { error } = validateAttedancePayload(payload)
@@ -16,7 +18,7 @@ export const saveAttendanceByClass = async (payload: Array<Omit<IAttendance, '_i
 		updateOne: {
 			filter: {
 				student: atd.student,
-				session: AttendanceSessionEnum[session],
+				session: AttendanceSessionEnum[session as keyof typeof AttendanceSessionEnum],
 				date: moment().format('YYYY-MM-DD')
 			},
 			update: atd,
@@ -43,26 +45,31 @@ export const getStudentAttendance = async (studentId: string, timeRangeOption?: 
 }
 
 // Reset form save attandance by class
-export const getClassAttendanceBySession = async (headTeacher: string, date: string | undefined, ss: string) => {
+export const getClassAttendanceBySession = async (headTeacher: string, date: string | undefined, session: string) => {
 	if (!!date && !moment(date).isValid()) throw createHttpError.BadRequest('Invalid date')
 	const classId = await ClassModel.findOne({ headTeacher: headTeacher }).distinct('_id')
 	console.log('classId', classId)
-	const studentsByClass = await StudentModel.find({ class: classId }).select('_id fullName').lean()
-	let attendanceOfClass = await AttendanceModel.find({
+	const studentsByClass = (await StudentModel.find({ class: classId }).select('_id fullName').lean()) as Array<
+		Pick<IStudent, '_id' | 'fullName'>
+	>
+	let attendanceOfClass = (await AttendanceModel.find({
 		student: { $in: studentsByClass.map((std) => std._id) },
 		date: moment(date).format('YYYY-MM-DD'),
-		session: AttendanceSessionEnum[ss]
+		session: AttendanceSessionEnum[session as keyof typeof AttendanceSessionEnum]
 	})
-
 		.populate({
 			path: 'student',
 			select: '_id fullName',
 			options: { sort: { fullName: 'asc' }, lean: true }
 		})
-		.select('-session -createdAt -updatedAt -date -_id')
-	if (!attendanceOfClass.length) attendanceOfClass = studentsByClass.map((std) => ({ student: std, isPresent: true }))
+		.select('-session -createdAt -updatedAt -date')) as Array<Pick<IAttendance, 'student' | 'isPresent'>>
+
+	if (!attendanceOfClass.length)
+		attendanceOfClass = studentsByClass.map(
+			(std) => ({ student: std, isPresent: true } as Pick<IAttendance, 'student' | 'isPresent'>)
+		)
 	return {
-		session: AttendanceSessionEnum[ss],
+		session: AttendanceSessionEnum[session as keyof typeof AttendanceSessionEnum],
 		date: moment(date).format('DD/MM/YYYY'),
 		attendances: attendanceOfClass
 	}
