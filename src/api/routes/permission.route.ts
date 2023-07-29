f043a96fa5cbd27c14464f26b122f43d0cdc242b
quanghiep03198 <quanghiep03198@gmail.com>
2023-07-29 17:28:49 +0700

    Update get permission by role router

--- src/api/routes/permission.route.ts
+++ src/api/routes/permission.route.ts
@@ -3,7 +3,7 @@ import * as permissionController from '../controllers/permission.controllers'
 import { checkIsHeadmaster, checkAuthenticated } from '../middlewares/authGuard.middleware'
 const router = express.Router()
 
-router.get('/permissions/get-by-roll', checkAuthenticated, permissionController.getPermissionByRole)
+router.get('/permissions/get-by-role', checkAuthenticated, permissionController.getPermissionByRole)
 router.post('/permissions', checkAuthenticated, checkIsHeadmaster, permissionController.createPermission)
 router.patch('/permissions/:id', checkAuthenticated, checkIsHeadmaster, permissionController.updatePermission)
 router.patch(
