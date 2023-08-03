17189016e460d982adff6b4c57d8dcd544f48ace
quanghiep03198 <quanghiep03198@gmail.com>
2023-08-03 23:19:47 +0700

    Fix: catch error if failed to get google api access token

--- src/configs/googleApis.config.ts
+++ src/configs/googleApis.config.ts
@@ -1,16 +1,16 @@
 import { drive_v3, google } from 'googleapis'
 import 'dotenv/config'
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_API_REFRESH_TOKEN, REDIRECT_URI, GOOGLE_API_KEY } = process.env
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_API_REFRESH_TOKEN, REDIRECT_URI } = process.env
 
 const oauth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URI)
 
 // Sets the auth credentials.
 oauth2Client.setCredentials({
-	refresh_token: GOOGLE_API_REFRESH_TOKEN!
+	refresh_token: <string>GOOGLE_API_REFRESH_TOKEN
 })
 // Get a non-expired access token, after refreshing if necessary
-oauth2Client.getAccessToken()
+oauth2Client.getAccessToken().catch((error) => console.log(`[ERROR] ::: <Google API> ${error.message}`))
 
 const drive: drive_v3.Drive = google.drive({
 	version: 'v3',
