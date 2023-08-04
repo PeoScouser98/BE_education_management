fee52526f11cc4858461bfb1abb08f83b60e3cfd
Maladroy <hoamgchilam@pm.me>
2023-08-04 23:05:08 +0700

    update: studentremark model, service, and controller

--- src/api/controllers/studentRemark.controller.ts
+++ src/api/controllers/studentRemark.controller.ts
@@ -1,13 +1,28 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from 'express'
 import useCatchAsync from '../../helpers/useCatchAsync'
 import * as StudentConductService from '../services/studentRemark.service'
 import { HttpStatusCode } from '../../configs/statusCode.config'
 
+// [PUT] /api/student-remark/:classId
 export const createStudentRemark = useCatchAsync(async (req: Request, res: Response) => {
 	const newStudenConductRemark = await StudentConductService.createStudentRemarkEntireClass(
 		req.body,
-		req.profile?._id!,
-		req.params.classId
+		req.profile?._id!
 	)
 	return res.status(HttpStatusCode.CREATED).json(newStudenConductRemark)
+})
+
+// [GET] /api/student-remark
+export const getStudentRemark = useCatchAsync(async (req: Request, res: Response) => {
+	const headTeacherID = req.profile?._id!
+	const remarks = await StudentConductService.getStudentRemarkByClass(headTeacherID)
+	return res.status(HttpStatusCode.OK).json(remarks)
+})
+
+//! FOR TESTING PURPOSE ONLY
+// [GET] /api/student-remark/gen/
+export const generateStudentRemark = useCatchAsync(async (req: Request, res: Response) => {
+	const generatedRemarks = await StudentConductService.generateFakeStudentRemark(req.profile?._id!)
+	return res.status(HttpStatusCode.CREATED).json(generatedRemarks)
 })
--- src/api/routes/studentRemark.route.ts
+++ src/api/routes/studentRemark.route.ts
@@ -4,6 +4,15 @@ import { checkAuthenticated, checkIsTeacher } from '../middlewares/authGuard.mid
 
 const router = express.Router()
 
-router.put('/student-remark/:classId', checkAuthenticated, checkIsTeacher, StudentRemarkController.createStudentRemark)
+router.put('/student-remark', checkAuthenticated, checkIsTeacher, StudentRemarkController.createStudentRemark)
+router.get('/student-remark', checkAuthenticated, checkIsTeacher, StudentRemarkController.getStudentRemark)
+
+//! FOR TESTING PURPOSE ONLY
+router.get(
+	'/student-remark/gen',
+	checkAuthenticated,
+	checkIsTeacher,
+	StudentRemarkController.generateStudentRemark
+)
 
 export default router
