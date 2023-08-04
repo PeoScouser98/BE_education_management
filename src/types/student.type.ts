5fce5cddea2884c5ba5d80c421d0fe6ff50003d2
Maladroy <hoamgchilam@pm.me>
2023-08-04 23:05:08 +0700

    update: studentremark model, service, and controller

--- src/types/student.type.ts
+++ src/types/student.type.ts
@@ -3,6 +3,7 @@ import { SoftDeleteDocument, SoftDeleteModel } from 'mongoose-delete'
 import { IUser } from './user.type'
 import { IClass } from './class.type'
 import { IAttendance } from './attendance.type'
+import { ISchoolYear } from './schoolYear.type'
 
 export interface IStudent extends Document {
 	_id: ObjectId
@@ -31,14 +32,14 @@ export enum StudentStatusEnum {
 export interface IStudentRemark extends Document {
 	_id: ObjectId
 	student: ObjectId | Pick<IStudent, '_id' | 'fullName' | 'class'>
+	schoolYear: string | ObjectId | Pick<ISchoolYear, '_id'>
 	conduct: string
 	proficiency: string
-	remark: string
 	isQualified: boolean
 	remarkedBy: string | ObjectId | Pick<IUser, '_id' | 'displayName'>
 }
 
-export interface IStudentDocument extends Omit<SoftDeleteDocument, '_id'>, IStudent {}
+export interface IStudentDocument extends Omit<SoftDeleteDocument, '_id'>, IStudent { }
 export type TStudentModel = Model<IStudentDocument>
 export type TSoftDeleteStudentModel = SoftDeleteModel<IStudentDocument, TStudentModel>
 export type TPaginatedStudentModel = PaginateModel<IStudentDocument>
